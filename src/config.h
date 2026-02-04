34339c2bef8aed830247ef906cc5d2fe0b777331
Emre Yilmaz <assademre@gmail.com>
2026-02-04 10:32:07 +0100

    add smoothness to reconstruction

---
+++ src/config.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <vector>
+
+constexpr int WIDTH = 32;
+constexpr int HEIGHT = 32;
+constexpr int TOTAL_ITERATIONS = 1000;
+
+
+inline std::vector<int> sensors = {50, 100};
+inline std::vector<double> noises = {0.5, 1.0, 5.0};
