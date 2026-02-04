08628a39e2f790eaa3f30ae57a51a7e5132cfd9c
Emre Yilmaz <assademre@gmail.com>
2026-02-04 18:55:42 +0100

    add config file

--- src/config.h
+++ src/config.h
@@ -1,10 +1,10 @@
 #pragma once
 #include <vector>
 
-constexpr int WIDTH = 32;
-constexpr int HEIGHT = 32;
-constexpr int TOTAL_ITERATIONS = 1000;
+constexpr int WIDTH = 64;
+constexpr int HEIGHT = 64;
+constexpr int TOTAL_ITERATIONS = 5000;
 
 
-inline std::vector<int> sensors = {50, 100};
-inline std::vector<double> noises = {0.5, 1.0, 5.0};
+inline std::vector<int> sensors = {100, 300, 500}; 
+inline std::vector<double> noises = {1.0, 3.0, 5.0};
